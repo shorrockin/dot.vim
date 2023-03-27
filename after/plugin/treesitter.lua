6e315e45af1b075a0ffbd238e0e4e9747ba612f3
Chris Shorrock <cs@stripe.com>
2023-03-27 10:45:47 -0700

    incremental selection for treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -12,6 +12,18 @@ require'nvim-treesitter.configs'.setup {
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
+  -- incremental selection allows for syntaticatly expanding selection regions, 
+  -- bound to ctrl-space by default
+  incremental_selection = {
+      enable = true,
+      keymaps = {
+          init_selection = '<c-space>',
+          node_incremental = '<c-space>',
+          scope_incremental = '<c-s>',
+          node_decremental = '<c-backspace>',
+      },
+  },
+
   highlight = {
     enable = true,
 
@@ -23,11 +35,10 @@ require'nvim-treesitter.configs'.setup {
             return true
         end
     end,
-
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages. 
+    -- Instead of true it can also be a list of languages.
     additional_vim_regex_highlighting = false,
   },
 }
