2f7a5743430e662e1e16ce5cdf2795e63bead996
Chris Shorrock <cs@stripe.com>
2023-03-27 10:38:27 -0700

    improved telescope bindings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,10 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = '[F]ind [f]iles in the project'})
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {desc = '[F]ind recently [o]pened files'})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = '[F]ind by [g]rep'})
+vim.keymap.set('n', '<leader>fw', builtin.grep_string, {desc = '[F]ind current [w]ord'})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = '[F]ind by [b]uffers'})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = '[F]ind by [h]elp'})
 
 -- allows for greping for a specific string, project-search
 vim.keymap.set('n', '<leader>ps', function()
