438d178c7b8083ae2a45cf388fecf4e286421cc8
Chris Shorrock <cs@stripe.com>
2023-05-01 14:15:12 -0700

    added documentation for shortcuts

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,12 +1,13 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = '[F]ind [f]iles in the project'})
-vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {desc = '[F]ind recently [o]pened files'})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = '[F]ind by [g]rep'})
-vim.keymap.set('n', '<leader>fw', builtin.grep_string, {desc = '[F]ind current [w]ord'})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = '[F]ind by [b]uffers'})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = '[F]ind by [h]elp'})
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = '[F]ind [f]iles in the project' })
+vim.keymap.set('n', '<leader>fo', telescope.oldfiles, { desc = '[F]ind recently [o]pened files' })
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = '[F]ind by [g]rep' })
+vim.keymap.set('n', '<leader>fp', telescope.grep_string, { desc = '[F]ind in [p]roject' })
+vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = '[F]ind by [b]uffers' })
+vim.keymap.set('n', '<leader>fh', telescope.help_tags, { desc = '[F]ind by [h]elp' })
+vim.keymap.set('n', '<leader>fs', telescope.lsp_document_symbols, { desc = '[F]ind by [s]ymbol' })
 
 -- allows for greping for a specific string, project-search
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("grep > ") });
+    telescope.grep_string({ search = vim.fn.input("grep > ") });
 end)
--- lua/cs/remap.lua
+++ lua/cs/remap.lua
@@ -1,31 +1,28 @@
+local nmap = function(keys, func, desc)
+    vim.keymap.set('n', keys, func, { desc = 'Remap: ' .. desc })
+end
+
+
 -- leader key to space
 vim.g.mapleader = " "
 
 -- simple write file, slightly faster
-vim.keymap.set("n", "<leader>w", vim.cmd.write)
+nmap("<leader>w", vim.cmd.write, "[W]rite")
 
 -- project view, opens upfile selection
-vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle)
-
--- allows you to select a bunch of text and move it around uing J and K
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+nmap("<leader>pv", vim.cmd.NvimTreeToggle, "[P]roject [V]iew")
 
 -- half page jumping keeps cursor in the middle, less dissorienting
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+nmap("<C-d>", "<C-d>zz", "Page [D]own")
+nmap("<C-u>", "<C-u>zz", "Page [U]p")
 
 -- similar to above, searching keeps terms in the middle of the screen
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
--- allows for pasting overtop off things from whatever is in your register,
--- while maintaining the contents on that register
-vim.keymap.set("x", "<leader>p", [["_dP]])
+nmap("n", "nzzzv", "[N]ext")
+nmap("N", "Nzzzv", "Previous")
 
 -- yank into the system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = 'Remap: [Y]ank To System Clipboard' })
+nmap("<leader>Y", [["+Y]], "[Y]ank To System Clipboard")
 
 -- don't use it, normally enters command mode
 vim.keymap.set("n", "Q", "<nop>")
@@ -44,10 +41,19 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- makes the current file executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = 'Remap: Make File E[x]ecutable' })
 
 -- toggles trouble buffer at the bottom of the screen
-vim.keymap.set("n", "<leader>tt", vim.cmd.TroubleToggle)
+nmap("<leader>tt", vim.cmd.TroubleToggle, "[T]rouble [T]oggle")
 
 -- toggles spell check on/off
-vim.keymap.set("n", "<leader>ts", function() vim.opt.spell = not (vim.opt.spell:get()) end)
+vim.keymap.set("n", "<leader>ts", function() vim.opt.spell = not (vim.opt.spell:get()) end,
+    { desc = 'Remap: Toggle [T]oggle [S]pell' })
+
+-- allows you to select a bunch of text and move it around uing J and K
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- allows for pasting overtop off things from whatever is in your register,
+-- while maintaining the contents on that register
+vim.keymap.set("x", "<leader>p", [["_dP]])
